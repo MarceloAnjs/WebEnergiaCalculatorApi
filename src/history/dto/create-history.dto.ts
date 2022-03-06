c16f32280a578b59d652ef8df778ac75900dbddd
asda <sadasdada@sdfasdas.con>
2022-03-06 15:59:57 -0300

    🚧 chore: Add History CRUD

--- src/history/dto/create-history.dto.ts
+++ src/history/dto/create-history.dto.ts
@@ -1,1 +1,19 @@
-export class CreateHistoryDto {}
+import { Prisma } from "@prisma/client";
+import { IsDecimal, IsJSON, IsString } from "class-validator";
+import { History } from "../entities/history.entity";
+
+export class CreateHistoryDto extends History{
+    @IsDecimal()
+    cost: string | number | Prisma.Decimal;
+    
+    @IsDecimal()
+    consumption: string | number | Prisma.Decimal;
+    
+    @IsDecimal()
+    fare: string | number | Prisma.Decimal;
+    
+    equipments: Prisma.InputJsonValue;
+    
+    @IsString()
+    userId: string;
+}
--- src/history/entities/history.entity.ts
+++ src/history/entities/history.entity.ts
@@ -1,1 +1,9 @@
-export class History {}
+import { Prisma } from "@prisma/client";
+
+export class History implements Prisma.HistoryUncheckedCreateInput{
+    cost: string | number | Prisma.Decimal;
+    consumption: string | number | Prisma.Decimal;
+    fare: string | number | Prisma.Decimal;
+    equipments: Prisma.InputJsonValue;
+    userId: string;
+}
