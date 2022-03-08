039621528900739060bdc4f39679482e03a1d339
asda <sadasdada@sdfasdas.con>
2022-03-07 22:44:10 -0300

    🩹 fix: Port heroku

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-<<<<<<< HEAD
   await app.listen(process.env.PORT || 3000);
-=======
-
   const config = new DocumentBuilder()
     .setTitle('Web Energia Calculator example')
     .setVersion('1.0')
@@ -17,7 +14,5 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
->>>>>>> jobs
 }
 bootstrap();
