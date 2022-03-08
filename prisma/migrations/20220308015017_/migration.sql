3caf2ff2719e3c753af30184ea688a4c5759d10f
asda <sadasdada@sdfasdas.con>
2022-03-07 22:52:47 -0300

    🩹 fix: Database

--- prisma/migrations/20220308015017_/migration.sql
+++ prisma/migrations/20220308015017_/migration.sql
@@ -1,19 +1,20 @@
 -- CreateTable
 CREATE TABLE `users` (
-    `id` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `users_email_key`(`email`),
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`email`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `equipment` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
-    `quantity` DECIMAL(65, 30) NOT NULL,
+    `quantity` INTEGER NOT NULL,
     `time` VARCHAR(191) NOT NULL,
     `power` DECIMAL(65, 30) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `userId` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
@@ -25,14 +26,15 @@ CREATE TABLE `history` (
     `cost` DECIMAL(65, 30) NOT NULL,
     `consumption` DECIMAL(65, 30) NOT NULL,
     `fare` DECIMAL(65, 30) NOT NULL,
-    `equipments` JSON NOT NULL,
+    `equipments` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `userId` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `equipment` ADD CONSTRAINT `equipment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `equipment` ADD CONSTRAINT `equipment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`email`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `history` ADD CONSTRAINT `history_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `history` ADD CONSTRAINT `history_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`email`) ON DELETE RESTRICT ON UPDATE CASCADE;
